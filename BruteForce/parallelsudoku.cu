3099ed1378a5cd5934125336aa7d1d086ce62a81
sophie <sophie4869@gmail.cm>
2017-05-11 14:47:34 -0400

    parallelsudoku

--- BruteForce/parallelsudoku.cu
+++ BruteForce/parallelsudoku.cu
@@ -4,3 +4,19 @@
 #include <driver_functions.h>
 #include "CycleTimer.h"
 
+
+
+__global__ void BGKernel(int *prev_boards, int* new_boards, int 
+	total_num_boards, int* empty_space_ind, int* empty_space_num) {
+
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	
+
+}
+
+void 
+Sudoku::main() {
+    BGKernel<<<boards, threadsPerBoard>>>(prev_boards, new_boards, 
+    	total_num_boards, empty_space_ind, empty_space_num);
+}
